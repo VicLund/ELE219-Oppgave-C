248c2c15233100dcdc7b91b396c9c6098e97f24d
VicLund <victoria.lund@outlook.com>
2021-05-12 13:39:07 +0200

    Add files via upload
    Added project files

---
+++ N_D_Flagg.h
@@ -0,0 +1,8 @@
+#include <avr/pgmspace.h>
+#ifndef N_D_Flagg_H__
+#define N_D_Flagg_H_
+
+extern const int Norsk[] PROGMEM;
+extern const int Dansk[] PROGMEM;
+
+#endif
---
+++ UIA_Scrolling.h
@@ -0,0 +1,44 @@
+#include <avr/pgmspace.h>
+#ifndef UIA_Scrolling_H__
+#define UIA_Scrolling_H_
+
+extern const int UIA1[] PROGMEM;
+extern const int UIA2[] PROGMEM;
+extern const int UIA3[] PROGMEM;
+extern const int UIA4[] PROGMEM;
+extern const int UIA5[] PROGMEM;
+extern const int UIA6[] PROGMEM;
+extern const int UIA7[] PROGMEM;
+extern const int UIA8[] PROGMEM;
+extern const int UIA9[] PROGMEM;
+extern const int UIA10[] PROGMEM;
+extern const int UIA11[] PROGMEM;
+extern const int UIA12[] PROGMEM;
+extern const int UIA13[] PROGMEM;
+extern const int UIA14[] PROGMEM;
+extern const int UIA15[] PROGMEM;
+extern const int UIA16[] PROGMEM;
+extern const int UIA17[] PROGMEM;
+extern const int UIA18[] PROGMEM;
+extern const int UIA19[] PROGMEM;
+extern const int UIA20[] PROGMEM;
+extern const int UIA21[] PROGMEM;
+extern const int UIA22[] PROGMEM;
+extern const int UIA23[] PROGMEM;
+extern const int UIA24[] PROGMEM;
+extern const int UIA25[] PROGMEM;
+extern const int UIA26[] PROGMEM;
+extern const int UIA27[] PROGMEM;
+extern const int UIA28[] PROGMEM;
+extern const int UIA29[] PROGMEM;
+extern const int UIA30[] PROGMEM;
+extern const int UIA31[] PROGMEM;
+extern const int UIA32[] PROGMEM;
+extern const int UIA33[] PROGMEM;
+extern const int UIA34[] PROGMEM;
+extern const int UIA35[] PROGMEM;
+extern const int UIA36[] PROGMEM;
+extern const int UIA37[] PROGMEM;
+
+
+#endif
